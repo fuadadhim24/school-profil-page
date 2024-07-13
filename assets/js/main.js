95500e9312d1b180d9b3e9d65ad04644db43e344
fuadadhim24 <fuadadhim24@gmail.com>
2024-07-13 15:39:01 +0700

    feat: add js for faq

--- assets/js/main.js
+++ assets/js/main.js
@@ -270,4 +270,13 @@
   }
 
   window.addEventListener("load", initSwiper);
+
+  /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
 })();
